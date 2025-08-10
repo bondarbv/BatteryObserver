792f6f4c006178e9f2445739709cb15fb505e517
Bohdan <bondarbv@icloud.com>
2025-08-10 19:14:30 +0300

    project init

---
+++ BatteryObserver/App/BatteryObserverApp.swift
@@ -0,0 +1,18 @@
+//
+//  BatteryObserverApp.swift
+//  BatteryObserver
+//
+//  Created by Bohdan Bondar on 10.08.2025
+//  
+//
+
+import SwiftUI
+
+@main
+struct BatteryObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BatteryObserver/Flows/MainViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewModel.swift
+//  BatteryObserver
+//
+//  Created by Bohdan Bondar on 10.08.2025
+//  
+//
+
+import Foundation
